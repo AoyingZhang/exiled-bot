4e3bd0fd69b1c024692f88424e07aa4c6cb9695e
Eric Kang <kang.eric.hi@gmail.com>
2023-12-26 11:54:55 -0500

    add an 8ball command

--- Modules/Fun.cs
+++ Modules/Fun.cs
@@ -73,5 +73,24 @@ namespace Discord_Bot
             await Context.Channel.SendMessageAsync(embeds: new[] { embed.Build() });
             
         }
+
+        [Command("8ball")]
+        [Summary("Ask the magic 8ball a question!")]
+        public async Task Eightball(string question) {
+            int rating = random.Next(0, 20);
+            string[] responses = {"It is certain. :white_check_mark:", "It is decidedly so. :white_check_mark:", "Without a doubt. :white_check_mark:", "Yes, definitely. :white_check_mark:", "You may rely on it. :white_check_mark:", "As I see it, yes. :white_check_mark:", "Most likely. :white_check_mark:", " Outlook good. :white_check_mark:", "Yes. :white_check_mark:", "Signs point to yes. :white_check_mark:", "Reply hazy, try again. :large_orange_diamond: ", "Ask again later. :large_orange_diamond: ", "Better not tell you now. :large_orange_diamond: ", "Cannot predict now. :large_orange_diamond: ", "Concentrate and ask again. :large_orange_diamond: ", "Do not count on it. :x:", "My reply is no. :x:", "My sources say no. :x:", "Outlook not so good. :x:", "Very doubtful. :x:"};
+            var embed = new EmbedBuilder
+            {
+                Title = question,
+                Description = responses[rating],
+                Timestamp = DateTimeOffset.Now,
+                Color = Color.Green,
+                Footer = new EmbedFooterBuilder()
+                    .WithText("Magic 8-ball")
+                    .WithIconUrl("https://vignette.wikia.nocookie.net/battlefordreamislandfanfiction/images/5/53/8-ball_my_friend.png/revision/latest?cb=20161109021012")
+            };
+            await Context.Channel.SendMessageAsync(embeds: new[] { embed.Build() });
+            
+        }
     }
 }    
